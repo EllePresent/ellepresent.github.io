714e2f12a3bfa25d608f3f5684ceb3c2bfc8e0cb
Risingvirtue <risingvirtue@berkeley.edu>
2018-01-07 05:06:55 -0800

    body gradient

--- bamboo.js
+++ bamboo.js
@@ -89,10 +89,86 @@ function animate(){
     t++;
 }
 
+var black = {r:0,g:0,b:0};
+var blue = {r:25, g: 25, b: 112};	
+var forest = {r:34, g: 139, b:34};
+var spring = {r:0, g: 255, b:127};
+var curr0 = {r:0,g:0,b:0};
+var curr1 = {r:25, g: 25, b: 112};	
+var finish = false;
+
+var count = 0;
+var deg = 45;
+function backgroundChange0() {
+	if (finish) {
+		clearInterval(interval0);
+	}
+	var dr0 = forest.r - black.r;
+	var dg0 = forest.g - black.g;
+	var db0 = forest.b - black.g;
+	var dr1 = 0;
+	var dg1 = 0;
+	var db1 = 0;
+	var dd = -90 / 20;
+	
+	
+	if (count < 19) {
+		curr0.r = Math.min(curr0.r + dr0 / 20, 34);
+		curr0.g = Math.min(curr0.g + dg0 / 20, 139);
+		curr0.b = Math.min(curr0.b + db0 / 20, 34);
+		//curr1.r = (curr1.r + dr1 / 20);
+		//curr1.g = (curr1.g + dg1 / 20);
+		//curr1.b = (curr1.b + db1 / 20);
+		deg = Math.max(-45, deg + dd);
+		count++;
+	} else {
+		count = 0;
+		finish = true;
+	}
+	//console.log(deg);
+	$('body').css('background', 'linear-gradient(' + deg+ 'deg,' + convertrgb(curr0) + ',' + convertrgb(curr1) + ')');
+}
+
+function backgroundChange1() {
+	if (finish) {
+		clearInterval(interval1);
+	}
+
+	var dr0 = black.r - forest.r;
+	var dg0 = black.g - forest.g;
+	var db0 = black.b - forest.b;
+	var dr1 = 0;
+	var dg1 = 0;
+	var db1 = 0;
+	var dd = 90 / 20;
+	
+	if (count < 19) {
+		curr0.r = Math.max(curr0.r + dr0 / 20, 0);
+		curr0.g = Math.max(curr0.g + dg0 / 20, 0);
+		curr0.b = Math.max(curr0.b + db0 / 20, 0);
+		//curr1.r = (curr1.r + dr1 / 20);
+		//curr1.g = (curr1.g + dg1 / 20);
+		//curr1.b = (curr1.b + db1 / 20);
+		deg = Math.min(45, deg +dd);
+		count++;
+	} else {
+		count = 0;
+		finish = true;
+	}
+	//console.log(deg);
+	$('body').css('background', 'linear-gradient(' + deg+ 'deg,' + convertrgb(curr0) + ',' + convertrgb(curr1) + ')');
+}
+
+function convertrgb(dict) {
+	return 'rgb(' + Math.round(dict.r) + ', ' + Math.round(dict.g) + ', ' + Math.round(dict.b) + ')'; 
+}
+
 function swap() {
+	
 	visible = !visible;
 	//cut away
 	$('#bCanvas').css("opacity", 0);
+	
 	if (visible) {
 		//resets bamboo drawing
 		$('#birthday').css('opacity', 0);
@@ -102,6 +178,13 @@ function swap() {
 		ctx.clearRect(0,0, canvas.width, canvas.height);
 		ctx.drawImage(bam,0,0, canvas.width, canvas.height);  
 		animate();
+		if (typeof interval1 != 'undefined') {
+			clearInterval(interval1);
+			count = 0;
+			finish = false;
+		
+		} 
+		interval0 = setInterval(backgroundChange0, 100);
 	} else {
 		//resets star drawing
 		$('#message').css('opacity', 0);
@@ -109,6 +192,19 @@ function swap() {
 		ctx.clearRect(0,0, canvas.width, canvas.height);
 		ctx.drawImage(b, 0,0, canvas.width, canvas.height);
 		update();
+		
+		if (typeof interval1 != 'undefined') {
+			clearInterval(interval1);
+			count = 0;
+			finish = false;
+		
+		} 
+		interval0 = setInterval(backgroundChange0, 100);
+		
+		if (typeof interval0 != 'undefined') {
+			clearInterval(interval0); count = 0; finish = false;
+		}
+		interval1 = setInterval(backgroundChange1, 100);
 	}
 	//fade back in
 	$('#bCanvas').fadeTo("slow", 1);
